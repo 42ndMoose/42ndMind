2b67be8af4cef903bbf418e11aed124cc898b5c4
42ndMoose <138173499+42ndMoose@users.noreply.github.com>
2026-06-12 14:11:35 -0500

    Set canonical active math

--- src/one-logic-math-v1.js
+++ src/one-logic-math-v1.js
@@ -4,56 +4,56 @@
 })(typeof globalThis !== 'undefined' ? globalThis : this, function() {
   'use strict';
 
-  const v = '1.1.0';
+  const v = '1.2.0';
 
   const F = Object.freeze([
-    'eq(1,1)',
-    'eq(B,1)',
-    'eq(L,1)',
-    'eq(q,1)',
-    'sub(L,B)',
-    'forall(q,imp(sub(q,B),eq(q,1)))',
-    'and(eq(D(q),S(Delta(q))),eq(q,1))',
-    'and(eq(Omega(q),not(S(Delta(q)))),eq(q,1))',
-    'and(eq(K(B,x),Bp),eq(Bp,1))',
-    'imp(E(q),eq(q,1))'
+    'B_t=N(U_t,R_t,X_t,C_t,O_t,F_t,P_t,G_t)',
+    'L_t=Pi_L(B_t)',
+    'q_t in U_t',
+    'norm(B_t)=1',
+    'norm(L_t)=1',
+    'norm(q_t)=1',
+    'I(x,t)=q_x_t',
+    'R(q_x_t,B_t)=r_x_t',
+    'X(q_x_t,r_x_t,B_t)=x_x_t',
+    'C(q_x_t,r_x_t,x_x_t,B_t)=c_x_t',
+    'O(q_x_t,r_x_t)=not(S(r_x_t))',
+    'F(B_t,q_x_t)=f_x_t',
+    'P(B_t,q_x_t,r_x_t,x_x_t,c_x_t)=p_x_t',
+    'G(B_t,q_x_t,r_x_t,x_x_t,c_x_t,O(q_x_t,r_x_t))=g_x_t',
+    'K(B_t,x)=N(B_t,q_x_t,r_x_t,x_x_t,c_x_t,O(q_x_t,r_x_t),f_x_t,p_x_t,g_x_t)',
+    'B_t1=K(B_t,x)',
+    'norm(B_t1)=1',
+    'P(B_t1)=1',
+    'E(B_t,phi)=Pi_E(F(B_t,phi))',
+    'P(E(B_t,phi))=1'
   ]);
 
   const A = Object.freeze([
-    ['eq', '1', '1'],
-    ['eq', 'B', '1'],
-    ['eq', 'L', '1'],
-    ['eq', 'q', '1'],
-    ['sub', 'L', 'B'],
-    ['forall', 'q', ['imp', ['sub', 'q', 'B'], ['eq', 'q', '1']]],
-    ['and', ['eq', ['D', 'q'], ['S', ['Delta', 'q']]], ['eq', 'q', '1']],
-    ['and', ['eq', ['Omega', 'q'], ['not', ['S', ['Delta', 'q']]]], ['eq', 'q', '1']],
-    ['and', ['eq', ['K', 'B', 'x'], 'Bp'], ['eq', 'Bp', '1']],
-    ['imp', ['E', 'q'], ['eq', 'q', '1']]
+    ['=', 'B_t', ['N', 'U_t', 'R_t', 'X_t', 'C_t', 'O_t', 'F_t', 'P_t', 'G_t']],
+    ['=', 'L_t', ['Pi_L', 'B_t']],
+    ['in', 'q_t', 'U_t'],
+    ['=', ['norm', 'B_t'], 1],
+    ['=', ['norm', 'L_t'], 1],
+    ['=', ['norm', 'q_t'], 1],
+    ['=', ['I', 'x', 't'], 'q_x_t'],
+    ['=', ['R', 'q_x_t', 'B_t'], 'r_x_t'],
+    ['=', ['X', 'q_x_t', 'r_x_t', 'B_t'], 'x_x_t'],
+    ['=', ['C', 'q_x_t', 'r_x_t', 'x_x_t', 'B_t'], 'c_x_t'],
+    ['=', ['O', 'q_x_t', 'r_x_t'], ['not', ['S', 'r_x_t']]],
+    ['=', ['F', 'B_t', 'q_x_t'], 'f_x_t'],
+    ['=', ['P', 'B_t', 'q_x_t', 'r_x_t', 'x_x_t', 'c_x_t'], 'p_x_t'],
+    ['=', ['G', 'B_t', 'q_x_t', 'r_x_t', 'x_x_t', 'c_x_t', ['O', 'q_x_t', 'r_x_t']], 'g_x_t'],
+    ['=', ['K', 'B_t', 'x'], ['N', 'B_t', 'q_x_t', 'r_x_t', 'x_x_t', 'c_x_t', ['O', 'q_x_t', 'r_x_t'], 'f_x_t', 'p_x_t', 'g_x_t']],
+    ['=', 'B_t1', ['K', 'B_t', 'x']],
+    ['=', ['norm', 'B_t1'], 1],
+    ['=', ['P', 'B_t1'], 1],
+    ['=', ['E', 'B_t', 'phi'], ['Pi_E', ['F', 'B_t', 'phi']]],
+    ['=', ['P', ['E', 'B_t', 'phi']], 1]
   ]);
 
-  const C = Object.freeze([
-    ['Gm', 'B', ['U', 'R', 'X', 'C', 'O', 'F', 'P', 'G']],
-    ['Gm', 'L', ['UL', 'RL', 'XL', 'CL', 'OL', 'FL', 'PL', 'GL']],
-    ['Gm', 'q', ['Uq', 'Rq', 'Xq', 'Cq', 'Oq', 'Fq', 'Pq', 'Gq']],
-    ['imp', ['Gm', 'z'], ['eq', 'z', '1']],
-    ['eq', ['sum', ['mul', 'wi', 'di']], '1']
-  ]);
-
-  const O = Object.freeze([
-    ['iota', 'x', 'q'],
-    ['rho', 'q'],
-    ['tau', 'q'],
-    ['pi', 'q'],
-    ['phi', 'B', 'q'],
-    ['K', 'B', 'x', 'Bp'],
-    ['eq', 'Bp', '1']
-  ]);
-
-  const M = Object.freeze({ v, F, A, C, O });
-
+  const M = Object.freeze({ v, F, A });
   function lines() { return F.slice(); }
   function textBlock() { return F.join('\n'); }
-
-  return Object.freeze({ VERSION: v, M, F, A, C, O, lines, textBlock });
+  return Object.freeze({ VERSION: v, M, F, A, lines, textBlock });
 });
